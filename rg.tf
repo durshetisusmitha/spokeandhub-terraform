68e6a425fd3cb91a43da1eecf7cb0bdaa3b18573
durshetisusmitha <82156615+durshetisusmitha@users.noreply.github.com>
2024-01-16 18:56:12 +0530

    Update rg.tf

--- rg.tf
+++ rg.tf
@@ -8,3 +8,14 @@ resource "azurerm_resource_group" "network_rg" {
     prod       = "false"
   }
 }
+
+resource "azurerm_resource_group" "security_rg" {
+  for_each = local.environments
+  name     = join("-", compact(["zsc", each.key, "security", "rg"]))
+  #  name        = format("zsc-%s-network-rg", each.key)
+  location = var.location
+  tags = {
+    created-by = "terraform"
+    prod       = "false"
+  }
+}
